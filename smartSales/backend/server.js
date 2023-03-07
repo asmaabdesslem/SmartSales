9ed54dbd0b8d6e16c04cbf27797ec277755ee49f
asmaabdesslem <49202966+asmaabdesslem@users.noreply.github.com>
2023-03-07 16:39:52 +0100

    project

---
+++ smartSales/backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import path from 'path';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const app = express();
+
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
